5bb0988ef1a28727015638e34ce6fc66ee72b937
HUZAIFA <huzaifawaseem97@gmail.com>
2024-08-09 15:07:42 +0500

    Tic-Tac-Toe: Implemented Tic-Tac-Toe game board and handled the button clicks from different players

---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Player({initialName, symbol, isActive}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [name, setName] = useState(initialName);
+
+    function handleClick() {
+        // setIsEditing(!isEditing); // schedule a state update. Not updating instantly 
+        setIsEditing(editing => !editing); // checks for the previous value of isEditing and updates instantly. Good Practice
+    }
+
+    function handleChange(event) {
+        setName(event.target.value);
+    }   
+
+    let nameContent = <span className="player-name">{name}</span>;
+    if (isEditing)
+        nameContent = <input type='text' required value={name} onChange={ handleChange }/>
+
+    return (
+        <li className={isActive ? 'active' : undefined} >
+            <span className="player">
+              { nameContent }
+              <span className="player-symbol">{symbol}</span>
+              <button onClick={()=>handleClick()}>{isEditing ? 'Save' : 'Edit'}</button>
+            </span>
+        </li>
+    );
+}
